b1980c8f06d8f2df960664628448dd55f2e8d670
Nicolas Araújo <nicolas.araujo@gertec.com.br>
2018-11-28 17:54:22 -0200

    src:  - Definindo nome do arquivo principal.

---
+++ src/source.py
@@ -0,0 +1,19 @@
+import sys
+
+print("\n\n***************************************")
+print("******* Explorando Marte - V2 *********")
+print("***************************************\n\n")
+
+print("Entre com os limites do quadrante: \n")
+x = input("Entre com o limite de X: ")
+y = input("Entre com o limite de Y: ")
+
+def setLimite( x, y ):
+    limiteX = x
+    limiteY = y
+
+def setPosicao( x, y, direcao ):
+    sonda.posX = x
+    sonda.posY = y
+    sonda.direcao = direcao
+
