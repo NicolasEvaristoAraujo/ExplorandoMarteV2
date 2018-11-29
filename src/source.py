d6dee484bb43a5305000d590e88da541c612db8b
Nicolas Araújo <nicolas.araujo@gertec.com.br>
2018-11-29 18:08:49 -0200

    - Correção de bugs nas coordenadas X e Y.

--- src/source.py
+++ src/source.py
@@ -1,33 +1,36 @@
 import sys
 import math
+import pdb
 from classe import Coordenadas
 
 cord = Coordenadas()
-direct = {'E': 270, 'W': 180, 'N': 0, 'S': 90}
+direct = {'E': 360, 'W': 180, 'N': 90, 'S': 270}
 
-#limiteX = int(limiteX)
-#limiteY = int(limiteY)
-#X       = int(X)
-#Y       = int(Y)
+angulo          = 0
+limiteX         = 0
+limiteY         = 0
+X               = 0
+Y               = 0
 
+# Interpreta os comandos de movimento
 def moveSonda( string ):
-    angulo = 0
-
     for letra in string:
         if letra == 'L':
             cord.direcao += 90
+            
         elif letra == 'R':
             cord.direcao -= 90
+            
         elif letra == 'M':
             angulo = ((360 * 2) * math.pi)/360
-            
-            cord.posX += math.cos(angulo)
+
+            cord.posX += int(math.cos(angulo))
             if cord.posX > limiteX:
                 cord.posX = limiteX
             elif cord.posX < 0:
                 cord.posX = 0
                 
-            cord.posY += math.sin(angulo)
+            cord.posY += int(math.sin(angulo))
             if cord.posY > limiteY:
                 cord.posY = limiteY
             elif cord.posY < 0:
@@ -49,11 +52,17 @@ def main():
 
     print("\n\n Entre com os limites superiores do quadrante ( X, Y )")
     limiteX, limiteY = input("X Y: ").split(" ")
+
+    limiteX = int( limiteX )
+    limiteY = int( limiteY )
     
     print("Entre com as coordenadas e a direcao da sonda (X, Y, D)")
     print("As posiveis direcoes sao N, W, S ou E")
     X, Y, D = input("X Y D: ").split(" ")
+
     cord.direcao = direct[D]
+    cord.posX = int(X)
+    cord.posY = int(Y)
 
     print("Entre com os comandos para mover a sonda (L, R ou M)")
     string = input(" ")
