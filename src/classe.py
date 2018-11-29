0223cca0cb48a398a22e7d6b2e17284367324a01
Nicolas Araújo <nicolas.araujo@gertec.com.br>
2018-11-29 16:32:24 -0200

    - Incluindo init self no arquivo classe, - Altaração da estrutura do arquivo classe, - Incluindo import math, - Mudanças no arquivo source

--- src/classe.py
+++ src/classe.py
@@ -1,24 +1,7 @@
-class sonda:
-
-    __dir = 0
-    __posX = 0
-    __posY = 0
+class Coordenadas():
+    posX    = 0
+    posY    = 0
+    direcao = 0
     
-
-    def set_dir( direcao ):
-        __dir = direcao
-
-    def get_dir():
-        return __dir
-
-    def set_posX( posX ):
-        __posX = posX
-
-    def get_posX():
-        return __posX
-
-    def set_posY( posY ):
-        __posY = posY
-
-    def get_posY():
-        return __posY
+    def __init__(self):    
+        pass
--- src/source.py
+++ src/source.py
@@ -1,32 +1,65 @@
 import sys
-from classe import sonda as sd
+import math
+from classe import Coordenadas
 
-print("\n\n***************************************")
-print("******* Explorando Marte - V2 *********")
-print("***************************************\n\n")
+cord = Coordenadas()
+direct = {'E': 270, 'W': 180, 'N': 0, 'S': 90}
 
-print("Entre com os limites do quadrante ( X, Y ) \n")
-limiteX, limiteY = input("( X, Y ): ").split(" ")
-string = input("Entre com a palavra: ")
+#limiteX = int(limiteX)
+#limiteY = int(limiteY)
+#X       = int(X)
+#Y       = int(Y)
 
-limiteX = int(limiteX)
-limiteY = int(limiteY)
-
-def setLimite( x, y ):
-    limiteX = x
-    limiteY = y
-
-def setPosicao( x, y, direcao ):
-    sd.set_posX(x)
-    sd.set_posY(y)
-    sd.set_dir(direcao)
-    
-def moveSonda( string  ):
+def moveSonda( string ):
     angulo = 0
-    
+
     for letra in string:
-        print(letra)
+        if letra == 'L':
+            cord.direcao += 90
+        elif letra == 'R':
+            cord.direcao -= 90
+        elif letra == 'M':
+            angulo = ((360 * 2) * math.pi)/360
+            
+            cord.posX += math.cos(angulo)
+            if cord.posX > limiteX:
+                cord.posX = limiteX
+            elif cord.posX < 0:
+                cord.posX = 0
+                
+            cord.posY += math.sin(angulo)
+            if cord.posY > limiteY:
+                cord.posY = limiteY
+            elif cord.posY < 0:
+                cord.posY = 0;
 
+        cord.direcao %= 360
+        if cord.direcao < 0:
+            cord.direcao += 360
 
-moveSonda(string)
+def printSondaPosition():
+    dirName = ("E", "N", "W", "S")
+    print("Posicao atual da sonda é")
+    print("%d %d %s \n" %(cord.posX, cord.posY, dirName[ int(cord.direcao / 90) ]))
 
+def main():
+    print("***************************************")
+    print("******* Explorando Marte - V2 *********")
+    print("***************************************")
+
+    print("\n\n Entre com os limites superiores do quadrante ( X, Y )")
+    limiteX, limiteY = input("X Y: ").split(" ")
+    
+    print("Entre com as coordenadas e a direcao da sonda (X, Y, D)")
+    print("As posiveis direcoes sao N, W, S ou E")
+    X, Y, D = input("X Y D: ").split(" ")
+    cord.direcao = direct[D]
+
+    print("Entre com os comandos para mover a sonda (L, R ou M)")
+    string = input(" ")
+    moveSonda(string)
+
+    printSondaPosition()
+
+if __name__ == '__main__':
+    main()
