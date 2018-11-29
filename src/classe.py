4dac4b53c51e86c2d2a97725f1c4d693c972c09f
Nicolas Araújo <nicolas.araujo@gertec.com.br>
2018-11-29 09:38:57 -0200

    Criando classe de controle, setters e getters.

---
+++ src/classe.py
@@ -0,0 +1,24 @@
+class sonda:
+
+    __dir = 0
+    __posX = 0
+    __posY = 0
+    
+
+    def set_dir( direcao ):
+        __dir = direcao
+
+    def get_dir():
+        return __dir
+
+    def set_posX( posX ):
+        __posX = posX
+
+    def get_posX():
+        return __posX
+
+    def set_posY( posY ):
+        __posY = posY
+
+    def get_posY():
+        return __posY
--- src/source.py
+++ src/source.py
@@ -1,19 +1,32 @@
 import sys
+from classe import sonda as sd
 
 print("\n\n***************************************")
 print("******* Explorando Marte - V2 *********")
 print("***************************************\n\n")
 
-print("Entre com os limites do quadrante: \n")
-x = input("Entre com o limite de X: ")
-y = input("Entre com o limite de Y: ")
+print("Entre com os limites do quadrante ( X, Y ) \n")
+limiteX, limiteY = input("( X, Y ): ").split(" ")
+string = input("Entre com a palavra: ")
+
+limiteX = int(limiteX)
+limiteY = int(limiteY)
 
 def setLimite( x, y ):
     limiteX = x
     limiteY = y
 
 def setPosicao( x, y, direcao ):
-    sonda.posX = x
-    sonda.posY = y
-    sonda.direcao = direcao
+    sd.set_posX(x)
+    sd.set_posY(y)
+    sd.set_dir(direcao)
+    
+def moveSonda( string  ):
+    angulo = 0
+    
+    for letra in string:
+        print(letra)
+
+
+moveSonda(string)
 
